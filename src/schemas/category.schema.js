bcb77bbee6e4828170440b2380bde6281f9b77c3
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2023-06-02 18:23:53 +0530

    refrence of graphql

--- src/schemas/category.schema.js
+++ src/schemas/category.schema.js
@@ -2,19 +2,22 @@ import gql from "graphql-tag";
 
 const categorySchema = gql`
   type Category {
-    _id: ID!
+    id: ID!
     name: String!
-    field: String!
+    products(filter: ProductsFilterInput): [Product]!
   }
-
+  input ProductsFilterInput {
+    id: ID
+    name: String
+  }
   type Query {
     getCategory(id: ID!): Category
     getAllCategory: [Category]
   }
 
   type Mutation {
-    createCategory(name: String!, field: String!): Category
-    updateCategory(id: ID!, name: String, field: String): Category
+    createCategory(name: String!): Category
+    updateCategory(id: ID!, name: String): Category
     deleteCategory(id: ID!): Category
   }
 `;
