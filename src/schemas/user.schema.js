5eacde689f1bc219617d95b6934da2576e896a96
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2023-06-02 15:03:24 +0530

    modifing the code & make it easy to understand

--- src/schemas/user.schema.js
+++ src/schemas/user.schema.js
@@ -1,45 +1,24 @@
 import gql from "graphql-tag";
-import { constraintDirectiveTypeDefs } from "graphql-constraint-directive";
-import { makeExecutableSchema } from "@graphql-tools/schema";
-import { applyMiddleware } from "graphql-middleware";
-import * as yup from "yup";
 
 const userSchema = gql`
-  ${constraintDirectiveTypeDefs}
-
   scalar DateTime
-
   input SignupInput {
-    email: String! @constraint(format: "email")
-    password: String! @constraint(minLength: 8)
+    email: String!
+    password: String!
     fname: String!
     lname: String
   }
-
   input LoginInput {
-    email: String! @constraint(format: "email")
-    password: String! @constraint(minLength: 8)
+    email: String!
+    password: String!
   }
-
-  type User {
-    _id: ID
-    email: String
-    password: String
-    fname: String
-    lname: String
-    createdAt: DateTime
-    updatedAt: DateTime
-  }
-
   type Query {
     getUsers(total: Int): [User]
     getUserById(id: ID!): User!
   }
-
   type JwtToken {
     token: String!
   }
-
   type UserWithToken {
     _id: String
     email: String
@@ -49,36 +28,10 @@ const userSchema = gql`
     updatedAt: DateTime
     userJwtToken: JwtToken
   }
-
   type Mutation {
     login(input: LoginInput): UserWithToken
     signup(input: SignupInput): UserWithToken
   }
 `;
 
-const schema = makeExecutableSchema({
-  typeDefs: [userSchema],
-});
-
-const validationMiddleware = {
-  async Mutation(resolve, parent, args, context, info) {
-    const { operation } = info;
-
-    if (operation === "signup") {
-      const schema = yup.object().shape({
-        email: yup.string().email().required(),
-        password: yup.string().min(8).required(),
-        fname: yup.string().required(),
-        lname: yup.string(),
-      });
-
-      await schema.validate(args.input, { abortEarly: false });
-    }
-
-    return resolve(parent, args, context, info);
-  },
-};
-
-const schemaWithMiddleware = applyMiddleware(schema, validationMiddleware);
-
-export default schemaWithMiddleware;
+export default userSchema;
