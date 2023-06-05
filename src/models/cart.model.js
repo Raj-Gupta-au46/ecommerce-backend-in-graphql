366bcb63492814f93c93820579f25ef7003c7f57
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2023-06-05 15:58:43 +0530

    adding new field to the model

--- src/models/cart.model.js
+++ src/models/cart.model.js
@@ -21,8 +21,17 @@ const cartSchema = new Schema(
           required: true,
           default: 1,
         },
+        price: {
+          type: Number,
+          required: true,
+        },
       },
     ],
+    total: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
   },
   { timestamps: true }
 );
