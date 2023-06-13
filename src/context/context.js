551d774079f2a1109f0b9fe6afbe86070f0366b1
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2023-06-13 14:49:06 +0530

    modifiy the code of jwt

--- src/context/context.js
+++ src/context/context.js
@@ -4,22 +4,29 @@ import { GraphQLError } from "graphql";
 export const getUser = async (token) => {
   try {
     if (token) {
-      const user = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
-      return user;
+      const decodedToken = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
+      return decodedToken;
     }
     return null;
   } catch (error) {
-    if (error.name == "JsonWebTokenError") {
+    if (error instanceof jwt.TokenExpiredError) {
+      throw new GraphQLError("Token expired. Please authenticate again.", {
+        extensions: {
+          code: "TOKEN_EXPIRED",
+        },
+      });
+    } else if (error instanceof jwt.JsonWebTokenError) {
       throw new GraphQLError("Invalid token. Please provide a valid JWT.", {
         extensions: {
-          code: "TOKEN_INVALID ",
+          code: "TOKEN_INVALID",
+        },
+      });
+    } else {
+      throw new GraphQLError("Error occurred while decoding token.", {
+        extensions: {
+          code: "TOKEN_DECODING_ERROR",
         },
       });
     }
-    throw new GraphQLError(`Error ${error}`, {
-      extensions: {
-        code: `ERROR ${error}`,
-      },
-    });
   }
 };
