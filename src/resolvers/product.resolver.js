6d5f31517a562176585bb2978c6bba968b63a7c9
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2023-06-13 18:18:36 +0530

    updating the code where admin can create product

--- src/resolvers/product.resolver.js
+++ src/resolvers/product.resolver.js
@@ -1,16 +1,24 @@
 import Product from "../models/product.model.js";
 import Category from "../models/category.model.js";
 import { uploadFile } from "../AWS/aws.js";
+import Admin from "../models/admin.model.js";
 
 const productResolvers = {
   Query: {
-    getProduct: async (parent, { id }) => {
+    getAllProducts: async (_, { page, limit }) => {
       try {
-        const product = await Product.findById(id).populate("category");
-        if (!product) {
-          throw new Error(`Product with ID ${id} not found.`);
-        }
-        return product;
+        const skip = (page - 1) * limit;
+        const totalCount = await Product.countDocuments();
+        const totalPages = Math.ceil(totalCount / limit);
+
+        const products = await Product.find().skip(skip).limit(limit);
+
+        return {
+          products,
+          totalPages,
+          currentPage: page,
+          totalCount,
+        };
       } catch (error) {
         throw new Error(error);
       }
@@ -27,28 +35,45 @@ const productResolvers = {
   Mutation: {
     createProduct: async (
       parent,
-      { name, description, price, categoryId, productImage }
+      { name, description, price, categoryId, productImage },
+      context
     ) => {
       try {
-        let productImageUrl = "";
-        if (productImage) {
-          const uploadedImage = await uploadFile(productImage);
-          productImageUrl = uploadedImage;
+        const { user } = context; // Assuming the authenticated user is available in the context
+        console.log("hit");
+        console.log(user);
+        console.log("end");
+        console.log(user.adminId);
+
+        const adminDetails = await Admin.findById(user.adminId);
+        console.log("admin");
+        console.log(adminDetails);
+        console.log("end");
+        console.log(adminDetails._id + "adminid");
+        console.log(user.adminId + "user id");
+        if (adminDetails._id && user.adminId) {
+          let productImageUrl = "";
+          if (productImage) {
+            const uploadedImage = await uploadFile(productImage);
+            productImageUrl = uploadedImage;
+          }
+
+          const newProduct = new Product({
+            name: name,
+            description: description,
+            price: price,
+            categoryId: categoryId,
+            productImage: productImageUrl,
+            createdAt: new Date().toISOString(),
+          });
+
+          const savedProduct = await newProduct.save();
+          // await savedProduct.populate('category').execPopulate();
+          // console.log(savedProduct)
+          return savedProduct;
+        } else {
+          throw new Error("Only admin can create a product");
         }
-
-        const newProduct = new Product({
-          name: name,
-          description: description,
-          price: price,
-          categoryId: categoryId,
-          productImage: productImageUrl,
-          createdAt: new Date().toISOString(),
-        });
-
-        const savedProduct = await newProduct.save();
-        // await savedProduct.populate('category').execPopulate();
-        // console.log(savedProduct)
-        return savedProduct;
       } catch (error) {
         throw new Error(error);
       }
