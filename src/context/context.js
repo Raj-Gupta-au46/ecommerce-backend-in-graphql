d898d14e05752bf084094bfcd6b88140589e8459
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2023-06-14 18:49:04 +0530

    removing the error of token and auth

--- src/context/context.js
+++ src/context/context.js
@@ -24,9 +24,8 @@ const getUser = async (token) => {
 };
 const context = async ({ req, res }) => {
   try {
-    console.log(req.path);
     const token = req.headers.authorization || "";
-    console.log(token);
+    //console.log(token);
     if (!token)
       throw new GraphQLError("User is not authenticated", {
         extensions: {
@@ -54,7 +53,79 @@ const context = async ({ req, res }) => {
     }
   } catch (error) {
     console.log(error);
+    //   throw new GraphQLError(`Error ${error}`, {
+    //     extensions: {
+    //       code: `ERROR_ ${error}`,
+    //       http: { status: 401 },
+    //     },
+    //   });
   }
 };
 
 export default context;
+
+// async function aggregationData({
+//   model,
+//   args,
+//   position = args?.length,
+//   sort,
+//   per_page,
+//   pageNo,
+//   isTotalData,
+// }) {
+//   try {
+//     if (sort) {
+//       args.splice(position, 0, {
+//         $sort: sort,
+//       });
+//     }
+
+//     if (
+//       typeof per_page === "number" &&
+//       typeof pageNo === "number" &&
+//       per_page > -1 &&
+//       pageNo > -1
+//     ) {
+//       const perPage = per_page;
+//       const skip = +perPage * pageNo;
+
+//       const totalCount = isTotalData
+//         ? await model.aggregate([
+//             ...args,
+//             {
+//               $count: "totalCount",
+//             },
+//           ])
+//         : undefined;
+
+//       args.splice(position + 1, 0, {
+//         $skip: skip,
+//       });
+//       args.splice(position + 2, 0, {
+//         $limit: per_page + 1,
+//       });
+//       const dataGet = await model.aggregate(args);
+
+//       const haveNextPage = Boolean(dataGet.length === Number(perPage) + 1);
+//       if (haveNextPage) {
+//         dataGet.pop();
+//       }
+
+//       return {
+//         results: dataGet,
+//         haveNextPage,
+//         pageNo: isTotalData ? pageNo : undefined,
+//         perPage: isTotalData ? per_page : undefined,
+//         totalCount: totalCount?.[0]?.totalCount,
+//       };
+//     } else {
+//       const dataGet = await model.aggregate(args);
+//       return {
+//         results: dataGet,
+//         haveNextPage: false,
+//       };
+//     }
+//   } catch (error) {
+//     throw error;
+//   }
+// }
