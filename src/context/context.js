5be9fcd25ee1eca3ca729babd92fa0bd4ed383c0
Raj-Gupta_au46 <raj.gupta07082000@gmail.com>
2023-06-19 17:44:19 +0530

    removing commented code

--- src/context/context.js
+++ src/context/context.js
@@ -25,7 +25,7 @@ const getUser = async (token) => {
 const context = async ({ req, res }) => {
   try {
     const token = req.headers.authorization || "";
-    //console.log(token);
+    console.log(token);
     if (!token)
       throw new GraphQLError("User is not authenticated", {
         extensions: {
@@ -52,80 +52,14 @@ const context = async ({ req, res }) => {
       }
     }
   } catch (error) {
-    console.log(error);
-    //   throw new GraphQLError(`Error ${error}`, {
-    //     extensions: {
-    //       code: `ERROR_ ${error}`,
-    //       http: { status: 401 },
-    //     },
-    //   });
+    // console.log(error);
+    // throw new GraphQLError(`Error ${error}`, {
+    //   extensions: {
+    //     code: `ERROR_ ${error}`,
+    //     http: { status: 401 },
+    //   },
+    // });
   }
 };
 
 export default context;
-
-// async function aggregationData({
-//   model,
-//   args,
-//   position = args?.length,
-//   sort,
-//   per_page,
-//   pageNo,
-//   isTotalData,
-// }) {
-//   try {
-//     if (sort) {
-//       args.splice(position, 0, {
-//         $sort: sort,
-//       });
-//     }
-
-//     if (
-//       typeof per_page === "number" &&
-//       typeof pageNo === "number" &&
-//       per_page > -1 &&
-//       pageNo > -1
-//     ) {
-//       const perPage = per_page;
-//       const skip = +perPage * pageNo;
-
-//       const totalCount = isTotalData
-//         ? await model.aggregate([
-//             ...args,
-//             {
-//               $count: "totalCount",
-//             },
-//           ])
-//         : undefined;
-
-//       args.splice(position + 1, 0, {
-//         $skip: skip,
-//       });
-//       args.splice(position + 2, 0, {
-//         $limit: per_page + 1,
-//       });
-//       const dataGet = await model.aggregate(args);
-
-//       const haveNextPage = Boolean(dataGet.length === Number(perPage) + 1);
-//       if (haveNextPage) {
-//         dataGet.pop();
-//       }
-
-//       return {
-//         results: dataGet,
-//         haveNextPage,
-//         pageNo: isTotalData ? pageNo : undefined,
-//         perPage: isTotalData ? per_page : undefined,
-//         totalCount: totalCount?.[0]?.totalCount,
-//       };
-//     } else {
-//       const dataGet = await model.aggregate(args);
-//       return {
-//         results: dataGet,
-//         haveNextPage: false,
-//       };
-//     }
-//   } catch (error) {
-//     throw error;
-//   }
-// }
